fc2d02a1d50ca497d48de1ff629cbea6a57605b0
sharmeenfatima23 <160642815+sharmeenfatima23@users.noreply.github.com>
2024-03-03 09:33:37 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,8 @@
+// Question1
+console.log('Hello world');
+
+
+// Question2
+let myName = 'ali';
+let message = 'is a good person';
+console.log(myName+" "+message);
---
+++ script.js
@@ -0,0 +1,8 @@
+// Question1
+//console.log('Hello world');
+
+
+// Question2
+//var myName = 'ali';
+//var message = 'is agood person';
+//console.log(myName +" " + message);
