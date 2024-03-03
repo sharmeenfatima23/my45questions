0bc53aaef95a947eea9cc71a9656cf3a1f1e7f0a
sharmeenfatima23 <160642815+sharmeenfatima23@users.noreply.github.com>
2024-03-03 09:57:06 +0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 // Question1
-console.log('Hello world');
+//console.log('Hello world');
 
 
 // Question2
-let myName = 'ali';
-let message = 'is a good person';
-console.log(myName+" "+message);
+//let myName = 'ali';
+//let message = 'is a good person';
+//console.log(myName+" "+message);
